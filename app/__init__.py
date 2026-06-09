04bb222cf11e31412e20e78430537248804fe950
hector <hektor7@gmail.com>
2026-06-09 16:28:15 +0200

    release: bump versión a 0.3.0 y workflow de build con PyInstaller (Win+Linux)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 """PyIssuesTracker - Cliente de escritorio Redmine multiplataforma."""
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __app_name__ = "PyIssuesTracker"
 __org__ = "pyissuestracker"
 __repo__ = "pyIssuesTracker"
