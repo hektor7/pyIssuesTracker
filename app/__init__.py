b524b726ca371760051281f7c530e644e2de3a70
hector <hektor7@gmail.com>
2026-06-27 12:09:40 +0200

    chore: bump version 0.3.19 → 0.3.20

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 """PyIssuesTracker - Cliente de escritorio Redmine multiplataforma."""
 
-__version__ = "0.3.19"
+__version__ = "0.3.20"
 __app_name__ = "PyIssuesTracker"
 __org__ = "pyissuestracker"
 __repo__ = "pyIssuesTracker"
